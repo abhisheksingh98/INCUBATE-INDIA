49c6e65225bebf68019b1afecab47b0c772442e2
abhisheksingh98 <writetoabhishek07@gmail.com>
2019-06-04 20:09:11 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { withScriptjs } from "react-google-maps";
+import Map from './Map';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const MapLoader = withScriptjs(Map);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <MapLoader
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDurZQBXjtSzKeieXwtFeGe-jhZu-HEGQU"
+      loadingElement={<div style={{ height: `100%` }} />}
+    />
+  );
+};
+
+render(<App />, document.getElementById('root'));
